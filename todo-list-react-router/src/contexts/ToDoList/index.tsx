fe96c41c60a06eace2fc385a709fa127d616b923
marvelopers <marvelopers@gmail.com>
2022-07-24 22:55:08 +0900

    add context

---
+++ todo-list-react-router/src/contexts/ToDoList/index.tsx
@@ -0,0 +1,50 @@
+import React, { createContext, useState, ReactNode, useEffect } from "react";
+
+interface Context {
+  toDoList: { id: number; toDo: string }[];
+  addToDo: (toDo: string) => void;
+  deleteItem: (id: number) => void;
+}
+
+const ToDoListContext = createContext<Context>({
+  toDoList: [],
+  addToDo: (): void => {},
+  deleteItem: (): void => {},
+});
+
+interface ToDoListProviderProps {
+  children: ReactNode;
+}
+
+const ToDoListProvider = ({ children }: ToDoListProviderProps) => {
+  const [toDoList, setToDoList] = useState<{ id: number; toDo: string }[]>([]);
+  const addToDo = (toDo: string) => {
+    if (toDo) {
+      setToDoList((prev) => [...prev, { id: prev.length + 1, toDo: toDo }]);
+    }
+  };
+
+  const deleteItem = (id: number) =>
+    setToDoList((prev) => prev.filter((item) => item.id !== id));
+
+  useEffect(() => {
+    const list = localStorage.getItem("ToDoList");
+    if (list) {
+      setToDoList(JSON.parse(list));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (toDoList.length > 0) {
+      localStorage.setItem("ToDoList", JSON.stringify(toDoList));
+    }
+  }, [toDoList]);
+
+  return (
+    <ToDoListContext.Provider value={{ toDoList, addToDo, deleteItem }}>
+      {children}
+    </ToDoListContext.Provider>
+  );
+};
+
+export { ToDoListContext, ToDoListProvider };
--- todo-list-react-router/src/index.tsx
+++ todo-list-react-router/src/index.tsx
@@ -1,6 +1,7 @@
-import App from 'components/App';
+// import App from 'Pages/App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from 'pages/App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
